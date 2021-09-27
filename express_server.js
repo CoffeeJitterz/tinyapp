bf2aa8ec53a68ccce5a48e2548b6575797f41452
Josh Millard <josh.millard10@gmail.com>
2021-09-27 19:10:20 -0400

    random string generator implimented, form implimented.

--- express_server.js
+++ express_server.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+function generateRandomString() {
+return Math.random().toString(20).substr(2, 6);
+};
+
 
 app.set("view engine", "ejs")
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -17,12 +31,14 @@ app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+res.render("urls_new");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
   res.render("urls_show", templateVars);
 });
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
